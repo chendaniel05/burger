645b6670d4de63fafc3e9d509845c97c20d8105d
Daniel Chengit add .git commit -m save <chendaniel05@gmail.com>
2018-09-21 15:04:04 -0400

    finished product

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cripsy Chicken Burger', false);
